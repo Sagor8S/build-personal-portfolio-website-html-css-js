218ed6b62745356802e953e76e2ada8d34884e45
Sagor8S <sagorhowlader.edu@gmail.com>
2026-02-24 21:29:04 +0600

    added an footer section code end

--- script.js
+++ script.js
@@ -62,5 +62,36 @@ circle.forEach(elem=>{
 });
 
 
+
+
 // mix portfolio code start here
 var mixer = mixitup('.portfolio-gallery');
+
+
+
+
+// activ menu code start here
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 <section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+// sticky navbar code start here
+
+const header = document.querySelector('header');
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
